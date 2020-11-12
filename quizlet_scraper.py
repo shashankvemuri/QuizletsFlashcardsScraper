7190ac8695444d1137973d4a89be637eb4b6c410
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-11-11 23:10:35 -0500

    create function

--- quizlet_scraper.py
+++ quizlet_scraper.py
@@ -1,32 +1,36 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
-# Set up quizlet scraper
 ## Example URL: https://quizlet.com/546386510/tapia-vocab-unit-2-flash-cards/
-url = input('Enter the Quizlet URL: ')
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-soup = BeautifulSoup(webpage, "html.parser")
 
-# find list of all span elements containing both the words and definitions
-termspan = soup.find_all('span', {'class' : 'TermText notranslate lang-en'})
+def getVocabulary(quizletURL):
+    req = Request(quizletURL, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, "html.parser")
 
-# create list of lines corresponding to element texts 
-allTerms = [span.get_text() for span in termspan]
+    # find list of all span element text containing both words/definitions
+    termspan = soup.find_all('span', {'class' : 'TermText notranslate lang-en'})
+    vocab = [span.get_text() for span in termspan]
 
-# new liss
-definitions = []
-terms = []
+    # new lists
+    definitions = []
+    terms = []
 
-# appending terms/definitions to new lists
-n = 0
-for every in allTerms:
-    if n % 2 == 1:
-        definitions.append(every)
-    else:
-        terms.append(every)
-    n += 1
+    # appending terms/definitions to new lists
+    n = 0
+    for vocabTerm in vocab:
+        if n % 2 == 1:
+            definitions.append(vocabTerm)
+        else:
+            terms.append(vocabTerm)
+        n += 1
 
-# printing out result
-for term, definition in zip(terms, definitions):
-    print (str(term) + ' - ' + str(definition))
+    # returning result
+    result = ""
+    for term, definition in zip(terms, definitions):
+        result += str(term) + ' - ' + str(definition + '\n')
+        
+    return result
+
+url = https://quizlet.com/546386510/tapia-vocab-unit-2-flash-cards/
+print(getVocabulary(url))
